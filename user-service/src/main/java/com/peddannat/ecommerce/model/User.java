ce2a3be7471530e2c630344b757e6e769559c91b
Peddanna <thokalapeddanna2121@gmail.com>
2026-05-27 23:34:48 +0530

    feat: add user service with MySQL setup

---
+++ user-service/src/main/java/com/peddannat/ecommerce/model/User.java
@@ -0,0 +1,31 @@
+package com.peddannat.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private LocalDateTime createdAt;
+}
